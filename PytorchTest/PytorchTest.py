b4ed58434aad7c8f66c31205bd267b1c969241e2
EinGespenstX <haoyx71@protonmail.com>
2020-02-17 23:41:45 +0800

    2020021701

--- PytorchTest/PytorchTest.py
+++ PytorchTest/PytorchTest.py
@@ -1,1 +1,23 @@
-print('fuck you')
+from __future__ import print_function
+import numpy
+import torch
+
+print(torch.cuda.device_count())
+print(torch.cuda.is_available())
+
+
+#dtype = torch.float
+device = torch.device("cuda")
+#device = torch.device("cpu")
+
+x = torch.ones(5, 5, device=device, requires_grad=True)
+print(x)
+
+y = x + 2
+print(y)
+
+z = y * y
+print(z)
+
+out=z.mean()
+print(out)
